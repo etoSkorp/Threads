68fda8d101bd35b280f1082646c149c776353a60
etoSkorp <almostround3@mail.ru>
2022-04-20 00:49:07 +0300

    Выполненные задания 1-3 по теме "Collections Framework". Так же выполнено задание по таймерам. Не стал делать отдельные классы, поскольку данные взяты из предыдущего задания по парсингу.

--- src/Main.java
+++ src/Main.java
@@ -13,50 +13,6 @@ import java.util.concurrent.*;
 public class Main {
     public static void main(String[] args) {
 
-        File file = new File("voyna.txt");
-
-        /*
-        Задание 1
-         */
-        Parser parser = new Parser();
-        ArrayList<String> sufferingWords = new ArrayList<>();
-
-        for (String elem : parser.parse(file)) {
-            if (elem.matches("^[Сс]трада(.*)")) {
-                sufferingWords.add(elem);
-                System.out.println(elem);
-            }
-        }
-        System.out.println(sufferingWords.size());
-
-        /*
-        Задание 2
-         */
-        ParserScanner parserScanner = new ParserScanner();
-        ArrayList<String> sufferingWordsScanner = new ArrayList<>();
-
-        for (String elem : parserScanner.parse(file)) {
-            if (elem.matches("^[Сс]трада(.*)")) {
-                sufferingWordsScanner.add(elem);
-                System.out.println(elem);
-            }
-        }
-        System.out.println(sufferingWordsScanner.size());
-
-        /*
-        Задание 3
-         */
-        for (int i = 1; i < 11; i++) {
-            for (int j = 1; j < 11; j++) {
-                if (j == 1)
-                    System.out.printf("%2d", i * j);  // Привет из python :)
-                else
-                    System.out.printf("%4d", i * j);  // Это, возможно, лишнее, но зато красиво :)
-            }
-            System.out.println();
-        }
-
-
         /*
         Задания по потокам
          */
